142f1383243d00f98c1e4bd05eff219822c3f3a1
mamedxanli <mamedxanli@gmail.com>
2016-05-09 17:31:45 +0200

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = '/home/mamedxanli/my-first-blog'
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+#application = get_wsgi_application()
+
+if path not in sys.path:
+  sys.path.append(path)
+  
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
